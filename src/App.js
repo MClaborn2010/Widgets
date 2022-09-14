cc436a28bc9da650195d17237dadacb818416931
Michael Claborn <mclaborn2010@gmail.com>
2022-09-14 13:44:39 -0500

    added wiki search that displays title and description. Button on right takes you to wiki article

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from "react";
 import Accordion from "./components/Accordion";
+import Search from "./components/Search";
 
 const items = [
   {
-    title: "What is React?",
-    content: "React is a frond end javascript framework",
+    title: "I'm an accordion!",
+    content: "Click me again and I will disappear :)",
   },
   {
-    title: "Why use React?",
-    content: "React is a favorite JS library among engineers",
+    title: "I'm also an accordion!",
+    content:
+      "Thanks to React and useState I was able to easily drop down and snap back for you!",
   },
   {
-    title: "How do you use React?",
-    content: "You use React by creating components.",
+    title: "I, too, am an accordion and play for the one true creator.",
+    content:
+      "It is by the grace of the React Gods that I am able to exist. I drop and snap back for the one true creator, ReactDOMinator.",
   },
 ];
 
 export default () => {
   return (
     <div>
-      <Accordion items={items} />
+      {/* <Accordion items={items} /> */}
+      <Search />
     </div>
   );
 };
